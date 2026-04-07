1b56d06742a9c5b1eaa7aba6ce487c07a839f383
LucasTameirao <lucas.atmartins@gmail.com>
2026-04-06 22:17:43 -0300

    terminar atividade

---
+++ src/MergeSort.java
@@ -0,0 +1,26 @@
+public class MergeSort<T extends Comparable<T>> implements IOrdenador<T>{
+
+    @Override
+    public T[] ordenar(T[] dados) {
+        
+    }
+
+    @Override
+    public long getComparacoes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getComparacoes'");
+    }
+
+    @Override
+    public long getMovimentacoes() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMovimentacoes'");
+    }
+
+    @Override
+    public double getTempoOrdenacao() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTempoOrdenacao'");
+    }
+    
+}
